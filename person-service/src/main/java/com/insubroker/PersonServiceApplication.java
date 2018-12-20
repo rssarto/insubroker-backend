a1e9cec08bf06e54014270c9806f0b0c99997de2
rssarto <ricardo.soares.sarto@gmail.com>
2018-12-20 21:27:58 -0200

    eureka server creation

--- person-service/src/main/java/com/insubroker/PersonServiceApplication.java
+++ person-service/src/main/java/com/insubroker/PersonServiceApplication.java
@@ -2,11 +2,13 @@ package com.insubroker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class PersonServiceApplication {
 
